0784407258d04e93cc8d17e299e13fe9efcfba91
Sofyan Hadi A <sofyan.h.ahmad@gmail.com>
2016-05-15 21:45:26 +0700

    Delete install-go-dependecies.sh

--- install-go-dependecies.sh
+++
@@ -1,7 +0,0 @@
-# Install all go dependecies
-
-go get github.com/gorilla/mux
-go get github.com/gorilla/websocket
-go get -v github.com/spf13/hugo
-go get -u -v github.com/spf13/hugo
-go install github.com/spf13/hugo/hugolib
